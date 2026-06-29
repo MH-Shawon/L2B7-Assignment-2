1cbbc1357d23a5dde42aca86c1fdff50ec5050a6
MH-Shawon <mohsinshawon18@gmail.com>
2026-06-29 20:23:52 +0600

    token verify completed

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/server.ts"],
 
-  format: ["esm"], // Keep this as ESM
+  format: ["esm"], // Keep ESM
 
   target: "esnext",
 
